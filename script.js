86cbe3f5e22333cd52b38be23cc02cf795b9aa36
Jason Kuffler <84805346+Jkuffler@users.noreply.github.com>
2022-02-20 05:12:22 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+//problem #1 
+
+var countEven = function(num) {
+
+  const digSum = (num) => {
+  const s = num.toString();
+      let sum = 0;
+      for(const char of s) {
+          sum += +char;
+      }
+      return sum
+    }
+  let count = 0;
+    for (let i=1; i<= num; i++) {
+        if (digSum(i)%2 === 0) count++;
+    }
+    return count;
+};
+// num.toString() offers such treats like this in the sense we are able to morph datatype and create a solution that works seamlessly with tiny bits of memory
+console.log(countEven(6/.1))
+console.log(countEven(999))
+console.log(countEven(5%9*4))
